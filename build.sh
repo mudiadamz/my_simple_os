47f0b8257ff21a40b6876067bd923e44fd14ca0c
Adam Mudianto <mudi.adamz@gmail.com>
2020-12-16 01:02:05 +0700

    ss

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ i686-elf-as boot.s -o boot.o
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 
-#cdrom image
+#cdrom image/iso
 mkdir -p isodir/boot/grub
 cp myos.bin isodir/boot/myos.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
