64b11d1cdfbeb1352ed41181e30592f24d2e28fb
Adam Mudianto <mudi.adamz@gmail.com>
2020-12-15 18:59:29 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ export PATH="$PREFIX/bin:$PATH"
 i686-elf-as boot.s -o boot.o
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+#this is copying the kernel to windows Desktop folder
+#you can comment it if you don't want to copy te kernel
 cp myos.bin /mnt/c/Users/$USER/Desktop/myos.bin
